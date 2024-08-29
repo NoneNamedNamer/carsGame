b4513a193136978f7d915cd0a601e31be4bb21f4
DOLOMAIN <70888845+NoneNamedNamer@users.noreply.github.com>
2024-08-29 22:08:31 +0300

    Update 1.1.2

---
+++ Assets/Scripts/GameOverScripts/Falling.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Falling : MonoBehaviour
+{
+    public Vector3 V3;
+    int q = -7;
+
+    void Update()
+    {
+        V3 = this.transform.position;
+        if (V3.y < q)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
--- Assets/Scripts/GameOverScripts/GameOver.cs
+++ Assets/Scripts/GameOverScripts/GameOver.cs
@@ -7,15 +7,6 @@ public class GameOver : MonoBehaviour
 {
     //public GameObject GameOverCanvas;
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "CarPlayer")
-        {
-            SceneManager.LoadScene(2);
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
-
     public void RestartGame()
     {
         SceneManager.LoadScene(1);
